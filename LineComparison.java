376dfa728a7a7b4c1bd02b39cca189693013ee8d
sonali2495 <sonaligadge24@gmail.com>
2021-08-22 10:22:43 +0530

    Display Message for Line Comparison

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Line_Comparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation");
+
+	}
+
+}
