89272183a2a9d6fc1c1fd7b00b6392815a519c3c
sonali2495 <sonaligadge24@gmail.com>
2021-08-22 22:41:45 +0530

    Updated Line Comparison Use Case 2

--- LineComparison.java
+++ LineComparison.java
@@ -1,15 +1,44 @@
 package com.bridgelabz.Line_Comparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	
 	
 
 	public static void main(String[] args) {
-		int x1=5, x2=100, y1=15, y2=200;
-		int length=(int)Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-		System.out.println("Length of the line (" + x1 + ", " + x2 + ") and (" + y1 + ", " + y2 + ") is " +length);
-	}
+		int x1, y1, x2, y2, x3, y3, x4, y4;
 
-	
-	
+		double length1, length2;
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter x1 point");
+		x1 = sc.nextInt();
+		System.out.println("enter y1 point");
+		y1 = sc.nextInt();
+		System.out.println("enter x2 point");
+		x2 = sc.nextInt();
+		System.out.println("enter y2 point");
+		y2 = sc.nextInt();
+		System.out.println("enter x3 point");
+		x3 = sc.nextInt();
+		System.out.println("enter y3 point");
+		y3 = sc.nextInt();
+		System.out.println("enter x4 point");
+		x4 = sc.nextInt();
+		System.out.println("enter y4 point");
+		y4 = sc.nextInt();
+
+		length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		length2 = Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+
+		System.out.println("Length of the line (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is " + length1);
+		System.out.println("Length of the line (" + x3 + ", " + y3 + ") and (" + x4 + ", " + y4 + ") is " + length2);
+		if (length1 == length2)
+			System.out.println("Both Line1 and Line2 are equal");
+		else
+			System.out.println("Both lines are not equal");
+	}
 }
